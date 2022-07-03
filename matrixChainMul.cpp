2765cce655878394ef7085fa932ea3cc5ae53902
Shashank Shandilya <97773334+ShashankS-02@users.noreply.github.com>
2022-07-03 20:50:15 +0530

    Final Matrix Chain Multiplication

--- matrixChainMul.cpp
+++ matrixChainMul.cpp
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define INF 9999
-int MatrixChainOrder(int arr[], int n)
+#include<bits/stdc++.h>
+using namespace std;
+#define inf 9999
+int matrix(int a[], int n)
 {
 	int p[n][n];
 	int i, j, k, l, q;
@@ -12,11 +12,11 @@ int MatrixChainOrder(int arr[], int n)
 		for (i = 1; i < n - l + 1; i++)
 		{
 			j = i + l - 1;
-			p[i][j] = INF;
+			p[i][j] = inf;
 			for (k = i; k <= j - 1; k++)
 			{
 			       q = p[i][k] + p[k + 1][j]
-					+ arr[i - 1] * arr[k] * arr[j];
+					+ a[i - 1] * a[k] * a[j];
 				if (q < p[i][j])
 					p[i][j] = q;
 			}
@@ -24,16 +24,17 @@ int MatrixChainOrder(int arr[], int n)
 	}
 	return p[1][n - 1];
 }
+
 int main()
 {
   int n;
-  printf("Enter the size of the array\n");
-  scanf("%d",&n);
-  int arr[n];
-  printf("Enter the array\n");
+  cout<<"Enter the size of the array\n";
+  cin>>n;
+  int a[n];
+  cout<<"Enter the array\n";
   for(int i=0;i<n;i++){
-  scanf("%d",&arr[i]);
+  cin>>a[i];
 }
-  printf("The Minimum number of matrix multiplications is %d ",MatrixChainOrder(arr, n));
+  cout<<"The Minimum number of matrix multiplications is: "<<matrixc(a, n);
 	return 0;
 }
